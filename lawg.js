ebd997defd7fe2fc324ae613f6c6ede1dd44382c
Marc Diethelm <marc.diethelm@namics.com>
2013-04-26 10:17:18 +0200

    _alert now handles NodeLists and Arrays, with deep conversion.

--- lawg.js
+++ lawg.js
@@ -9,46 +9,51 @@
 		,c
 		,isElement
 		,isjQuery
+		,isArray
 	;
-	
-	function concatArgs(args) {
-		var str = ''
+
+	function concatArgs(args, nestedCall) {
+		var aStr = []
 			,i
 			,arg
-			,delim = '  '
+			,delim = nestedCall ? ', ' : ',\n'
 		;
 
 		for (i = 0; i<args.length; i++) {
 			arg = args[i];
 			if (arg !== undefined && arg !== null) {
-				
-				// if this is an DOM element node, show an appropriate representation. 
-				if (isElement(arg))
-					str += elementToString(arg);
+
+				if (isArray(arg))
+					aStr.push('[' + arguments.callee(Array.prototype.slice.call(arg), true) + ']');
+				// if this is an DOM element node, show an appropriate representation.
+				else if (isElement(arg))
+					aStr.push( elementToString(arg) );
 				// if this is a jQuery object, use the log method plugin defined below
 				else if (isjQuery(arg))
-					str += jQueryToString(arg);
+					aStr.push(jQueryToString(arg));
+				// if it's a NodeList 'convert' it to an array and then... magic
+				else if (arg.toString() == '[object NodeList]')
+					aStr.push('NodeList[' + arguments.callee(Array.prototype.slice.call(arg), true) + ']');
 				// for everything else use native JS toString methods. Or fail miserably! :((
 				else
-					str += arg.toString();
+					aStr.push(arg.toString());
 			 // arg is either undefined or null. Let the JS engine convert it to string.
 			} else
-				str += arg;
-			
-			str += delim;
+				aStr.push(arg + '');
+
 		}
-		return str;
+		return aStr.join(delim);
 	}
-	
+
 	function elementToString(elem) {
 		var htmlId = ''
 		    ,htmlClass = ''
 		;
-		elem.id && (htmlId = '#' + elem.id); 
+		elem.id && (htmlId = '#' + elem.id);
 		elem.className && (htmlClass = '.' + elem.className.replace(/\s/g, '.')); // replace any white space with a '.'
 		return elem.tagName + htmlId + htmlClass;
 	}
-	
+
 	function jQueryToString($obj) {
 		var i
 			,stringArray = []
@@ -56,16 +61,18 @@
 		for (i=0; i<$obj.length; i++) {
 			stringArray.push( elementToString($obj[i]) );
 		}
-		
+
 		return '$('+ stringArray.join(', ') +')';
 	}
-	
+
 	//Resulting function returns true if param is a DOM element
-	if (typeof HTMLElement === 'object')
+	if (typeof HTMLElement == 'function') {
 		isElement = function(obj) { return obj instanceof HTMLElement }
-	else
+	}
+	else {
 		isElement = function(obj) { return typeof obj == 'object' && obj.nodeType === 1 && typeof obj.nodeName==='string' }
-	
+	}
+
 	// Resulting function returns true if param is a jQuery object
 	try {
 		$() instanceof jQuery; // abusing try/catch for flow control. Old crappy browsers (I'm lookin at you IE) will throw.
@@ -73,6 +80,10 @@
 	} catch (e) {
 		isjQuery = function(obj) { return 'jquery' in obj && typeof obj.jquery == 'string' }
 	}
+
+	isArray = Array.isArray || function(obj) {
+		return toString.call(obj) == '[object Array]';
+	};
 	
 	if (w.console) {
 		c = console;
